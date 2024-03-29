83fa2d8d8a725ac025111d9dc0ede30aec4aae89
CSP02 <chandrapilla2002@gmail.com>
2024-03-29 20:28:13 +0530

    updated website

--- script.js
+++ script.js
@@ -30,6 +30,36 @@ window.onload = () => {
         toggler.toggleSlide("menu", "right", .2)
     })
 
+    // const effect = setInterval(parallaxEffect, 1000)
+
+    window.addEventListener("mouseover", (scroll) => {
+        const background = document.getElementsByTagName("svg")[0]
+        const firstSvg = [...background.getElementsByClassName("first")];
+        const secondSvg = [...background.getElementsByClassName("second")];
+        const thirdSvg = [...background.getElementsByClassName("third")];
+        const offset = 5
+
+        firstSvg.forEach(path => {
+            const x = (window.innerWidth - scroll.pageX * 1) / offset;
+            const y = (window.innerHeight - scroll.pageY * 1) / offset;
+
+            path.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+        secondSvg.forEach(path => {
+            const x = (window.innerWidth - scroll.pageX * 2) / offset;
+            const y = (window.innerHeight - scroll.pageY * 2) / offset;
+
+            path.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+        thirdSvg.forEach(path => {
+            const x = (window.innerWidth - scroll.pageX * 3) / offset;
+            const y = (window.innerHeight - scroll.pageY * 3) / offset;
+
+            path.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        });
+        // clearInterval(effect)
+    })
+
     const cdnInp = document.getElementById("cdn_inp")
     cdnInp.addEventListener("click", () => {
         const cdn = cdnInp.innerText
@@ -37,11 +67,37 @@ window.onload = () => {
         navigator.clipboard.writeText(cdn).then(callback => {
             cdnInp.style.border = "1px solid green"
             document.getElementById("copied").innerText = "Copied"
-            
+
             setTimeout(() => {
                 cdnInp.style = ""
                 document.getElementById("copied").innerText = "Click to copy"
             }, 3000)
         })
+    })
+}
+
+function parallaxEffect() {
+    const background = document.getElementsByTagName("svg")[0]
+    const firstSvg = [...background.getElementsByClassName("first")];
+    const secondSvg = [...background.getElementsByClassName("second")];
+    const thirdSvg = [...background.getElementsByClassName("third")];
+
+    firstSvg.forEach(path => {
+        const x = Math.random() * 100
+        const y = Math.random() * 100
+
+        path.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+    secondSvg.forEach(path => {
+        const x = Math.random() * 100
+        const y = Math.random() * 100
+
+        path.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+    thirdSvg.forEach(path => {
+        const x = Math.random() * 100
+        const y = Math.random() * 100
+
+        path.style.transform = `translateX(${x}px) translateY(${y}px)`;
     })
 }
